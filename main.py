87bfcfc4806f9b22fb2c4e60d8c618b0d6df02e1
Jamula Krishna Rao <krishnarao@opentext.com>
2026-03-11 23:27:43 +0530

    aa

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+def main():
+    print("Hello from langchain-course!")
+    print(f"Your API key is: {os.getenv('OPENAI_API_KEY')}")
+    print(os.environ.get("OPENAI_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
